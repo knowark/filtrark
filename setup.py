46ff4df9ac8bb500867cb4285be0b1705eac1e6b
Esteban Echeverry <eecheverry@nubark.com>
2018-03-19 09:12:18 -0500

    Push version to 0.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='filtrark',
-    version='0.2.0',
+    version='0.2.1',
     description="Build filter clauses from instruction lists",
     long_description=readme + '\n\n' + history,
     author="Esteban Echeverry",
