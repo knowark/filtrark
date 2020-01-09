3bb5b13f39a37c986417d998f18f225dd002f402
eecheverry <eecheverry@nubark.com>
2020-01-09 09:42:08 -0500

    Rename %s placeholder name to 'string'

--- tests/test_sql_parser.py
+++ tests/test_sql_parser.py
@@ -7,7 +7,7 @@ from filtrark.sql_parser import SqlParser
 class TestSqlParser(unittest.TestCase):
 
     def setUp(self):
-        self.parser = SqlParser(placeholder='text')
+        self.parser = SqlParser(placeholder='string')
 
     def test_sql_parser_object_creation(self):
         self.assertTrue(isinstance(self.parser, SqlParser))
